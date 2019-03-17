9d2bbbd76bb7a568a89e1c4cf3e0fb8fc7c60309
Samy Ayoub <samyayoub@gmail.com>
2019-03-17 23:06:08 +0000

    Handle form submission

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,11 +1,22 @@
 class PlacesController < ApplicationController
 
     def index
-        @places = Place.all.page(params[:page]).per_page(2)
+        @places = Place.all.page(params[:page]).per_page(3)
     end
 
     def new
         @place = Place.new
+    end
+
+    def create
+        Place.create(place_params)
+        redirect_to root_path
+    end
+    
+    private
+    
+    def place_params
+        params.require(:place).permit(:name, :description, :address)
     end
 
 end
