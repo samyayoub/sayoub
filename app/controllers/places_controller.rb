3a03cb4f71d4f94a8472a3be84569d0dbceb0058
Samy Ayoub <samyayoub@gmail.com>
2019-03-22 22:06:37 +0000

    Destroy permissions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
 
-    before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
     
     def index
         @places = Place.all.page(params[:page]).per_page(3)
@@ -32,13 +32,17 @@ class PlacesController < ApplicationController
         if @place.user != current_user
             return render plain: 'You Shall Not Pass!', status: :forbidden
         end
-        
+
         @place.update_attributes(place_params)
         redirect_to root_path
     end
 
     def destroy
         @place = Place.find(params[:id])
+        if @place.user != current_user
+            return render plain: 'You Shall Not Pass!', status: :forbidden
+        end
+        
         @place.destroy
         redirect_to root_path
     end
